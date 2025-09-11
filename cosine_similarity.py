e7398a73be13042bb4be500e72a4bfb78108ea50
ahaibel <ahaibel@gmail.com>
2025-09-11 13:53:17 -0400

    a

---
+++ cosine_similarity.py
@@ -0,0 +1,22 @@
+from scipy.spatial.distance import cdist
+import numpy as np
+
+# EuAl3Cu2
+ref_a = 5.419676971280253
+ref_b = 5.419676971280253
+ref_c = 4.07009126
+ref_v = 103.53366382224992
+
+tes_a = 1.0474285366
+tes_b = 1.0474285366
+tes_c = 3.7621771
+tes_v = 100.9127948
+
+del_a = ref_a - tes_a
+del_b = ref_b - tes_b
+del_c = ref_c - tes_c
+del_v = ref_v - tes_v
+
+curr_deltas = [[]]
+
+dists = cdist(curr_deltas, batch, "euclidean")
